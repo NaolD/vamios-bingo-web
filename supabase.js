198c1722ebbb9bf43603a0d3ccacd372eddb227a
NaolD <naoldebele10@gmail.com>
2026-08-18 19:50:51 +0300

    Use Supabase publishable key

--- supabase.js
+++ supabase.js
@@ -6,26 +6,16 @@
 const SUPABASE_URL =
     "https://ymmeeppimzyiunscjheh.supabase.co";
 
-const SUPABASE_ANON_KEY =
-    "sb_publishable_d2mVCrvtCDs-JA6ShYkYf1Q_FcJ26AhB";
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_d2mVCrvtCDs-JA6ShkYf1Q_FcJ26AhB";
 
-// The CDN already provides window.supabase.
-// Create the actual Supabase client without
-// declaring another global variable named supabase.
-
-window.vamiosSupabase =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_ANON_KEY
-    );
-
-// Make all existing VAMIOS files use:
-// supabase.from(...)
-// supabase.auth(...)
-// etc.
 
 window.supabase =
-    window.vamiosSupabase;
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
+
 
 console.log(
     "VAMIOS SUPABASE CLIENT READY"
